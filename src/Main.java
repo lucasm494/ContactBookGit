aa909d330f75cf38ba0e7085533eacd367b1882e
Tomas <tma.simoes@campus.fct.unl.pt>
2024-09-14 11:54:39 +0100

    Added constant for new commands

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,9 @@ public class Main {
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
     public static final String QUIT           = "Q";
+    public static final String GET_NUMBER = "GN";
+    public static final String Repeated= "EP";
+
 
     //Constantes que definem as mensagens para o utilizador
     public static final String CONTACT_EXISTS = "contactBook.Contact already exists.";
